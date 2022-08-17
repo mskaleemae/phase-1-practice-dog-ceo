dc04c0fcea4c9f73af4ed7588f172011c3f76dd9
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-08-17 09:54:23 -0400

    commit tree 16f104477985e044a5a98450a9bfdff9179841a3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.getElementById('dog-image-container');
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(res => {
+            res.message.forEach((imgSrc) => {
+                const img = document.createElement('img');
+                img.src = imgSrc;
+                div.appendChild(img);
+            })
+        });
+});
+<div id="dog-image-container">
+     
+    </div>
